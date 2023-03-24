c989718af3d8c08905c7a74df9374514dd40a880
JustWaveThings <justwavethings@proton.me>
2023-03-24 11:59:47 -0400

    Add more examples to show the sort works correctly

--- merge_sort.js
+++ merge_sort.js
@@ -32,3 +32,11 @@ function mergeTwoArrays(array1, array2) {
 }
 
 console.log(mergeSort(inputArray));
+
+const unsorted1 = [93, 151, 3, 20, 463, 155, 167, 34, 13];
+const unsorted2 = [-3, 0, 104, 10456, 4, 790, 90, -90];
+const unsorted3 = [10, 4.5, 3.7, 1.1, 0.3, 9.99, 8, 4, 4.7];
+
+console.log(mergeSort(unsorted1)); //[3, 13, 20, 34, 93, 151, 155, 167, 463]
+console.log(mergeSort(unsorted2)); //[-90, -3, 0, 4, 90, 104, 790, 10456]
+console.log(mergeSort(unsorted3)); //[0.3, 1.1, 3.7, 4, 4.5, 4.7, 8, 9.99, 10]
