a9c62f8d3e1b8fe94aae7e935626073077af07f8
JustWaveThings <justwavethings@proton.me>
2023-03-26 18:56:15 -0400

    Driver script complete, exercise complete. Looks correct.

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -1,4 +1,3 @@
-// the nodes of the tree
 class Node {
 	constructor(value) {
 		this.value = value;
@@ -7,7 +6,6 @@ class Node {
 	}
 }
 
-// the actual tree class from which nodes are created and the buildTree method is called
 class Tree {
 	constructor(array) {
 		this.array = buildTree.removeDuplicates(array);
@@ -15,7 +13,7 @@ class Tree {
 		this.root = treeBuild.build(this.array);
 	}
 }
-// apty named class to build the tree.
+
 class buildTree {
 	constructor(array) {
 		this.array = array;
@@ -26,7 +24,7 @@ class buildTree {
 			return a - b;
 		});
 	}
-	// remove duplicates from array to reduce complexity
+
 	static removeDuplicates(array) {
 		const sorted = buildTree.sortArray(array);
 		let uniqueArray = sorted.filter(function (item, pos) {
@@ -36,7 +34,6 @@ class buildTree {
 		return uniqueArray;
 	}
 
-	// build the tree and make recursive calls until base case of unique array length === 0 is met
 	build(uniqueArray) {
 		if (uniqueArray.length === 0) return null;
 		let mid = Math.floor(uniqueArray.length / 2);
@@ -131,17 +128,6 @@ function getSuccessorRecursive(delNode, current, parent, isLeftChild) {
 	return getSuccessorRecursive(current, current.left, current, true);
 }
 
-function preOrder(node, output = []) {
-	if (node === null) {
-		return output;
-	} else {
-		output.push(node.value);
-		preOrder(node.left, output);
-		preOrder(node.right, output);
-		return output;
-	}
-}
-
 function depth(bst, value) {
 	function depthRecursive(node, value, currentDepth) {
 		if (node === null) {
@@ -172,6 +158,46 @@ function getHeight(node) {
 	return Math.max(leftHeight, rightHeight) + 1;
 }
 
+function isBalanced(bst) {
+	if (bst === null) return true;
+	const leftHeight = getHeight(bst.left);
+	const rightHeight = getHeight(bst.right);
+	if (Math.abs(leftHeight - rightHeight) > 1) return false;
+	return isBalanced(bst.left) && isBalanced(bst.right);
+}
+
+function rebalance(bst) {
+	const array = inOrder(bst);
+	const newTree = new Tree(array);
+	return newTree;
+}
+
+function levelOrder(node, funct) {
+	if (node === null) return;
+	const queue = [node];
+	levelOrderRecursive(queue, funct);
+}
+
+function levelOrderRecursive(queue, funct) {
+	if (queue.length === 0) return;
+	const node = queue.shift();
+	funct(node);
+	if (node.left !== null) queue.push(node.left);
+	if (node.right !== null) queue.push(node.right);
+	levelOrderRecursive(queue, funct);
+}
+
+function preOrder(node, output = []) {
+	if (node === null) {
+		return output;
+	} else {
+		output.push(node.value);
+		preOrder(node.left, output);
+		preOrder(node.right, output);
+		return output;
+	}
+}
+
 function inOrder(node, output = []) {
 	if (node === null) return output;
 	inOrder(node.left, output);
@@ -188,7 +214,78 @@ function postOrder(node, output = []) {
 	return output;
 }
 
-//const test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+function driverScript() {
+	function randomNumberArrayRecursive(size) {
+		if (size === 0) return [];
+		const random = Math.floor(Math.random() * 100);
+		return [random, ...randomNumberArrayRecursive(size - 1)];
+	}
+	const testArray = randomNumberArrayRecursive(10);
+	console.log(testArray);
+	console.log('-----------------------');
+	const test = new Tree(testArray);
+	prettyPrint(test.root);
+	console.log('-----------------------');
+	const isBalancedTest = isBalanced(test.root);
+	console.log(isBalancedTest, 'isBalanced');
+	console.log('-----------------------');
+	console.log('Level Order');
+	const level = levelOrder(test.root, node => console.log(node.value));
+	console.log(level);
+	console.log('-----------------------');
+	console.log('Pre Order');
+	const pre = preOrder(test.root);
+	console.log(pre);
+	console.log('-----------------------');
+	console.log('In Order');
+	const inOrderTest = inOrder(test.root);
+	console.log(inOrderTest);
+	console.log('-----------------------');
+	console.log('Post Order');
+	const post = postOrder(test.root);
+	console.log(post);
+	console.log('-----------------------');
+	console.log('Depth');
+	const depthTest = depth(test, testArray[3]);
+	console.log(depthTest, ' - ', testArray[3]);
+	console.log('-----------------------');
+	console.log('Height');
+	const heightTest = height(test.root, testArray[3]);
+	console.log(heightTest, ' - ', testArray[3]);
+	console.log('-----------------------');
+	const unbalanceAdd = [
+		8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000,
+		19000, 20000,
+	];
+	unbalanceAdd.forEach(num => insert(test.root, num));
+	prettyPrint(test.root);
+	console.log('-----------------------');
+	const isBalancedTest2 = isBalanced(test.root);
+	console.log(isBalancedTest2, 'isBalanced');
+	console.log('-----------------------');
+	const rebalanceTest = rebalance(test.root);
+	prettyPrint(rebalanceTest.root);
+	console.log('-----------------------');
+	const isBalancedTest3 = isBalanced(rebalanceTest.root);
+	console.log(isBalancedTest3, 'isBalanced');
+	console.log('-----------------------');
+	console.log('Pre Order');
+	const pre2 = preOrder(test.root);
+	console.log(pre2);
+	console.log('-----------------------');
+	console.log('In Order');
+	const inOrderTest2 = inOrder(test.root);
+	console.log(inOrderTest2);
+	console.log('-----------------------');
+	console.log('Post Order');
+	const post2 = postOrder(test.root);
+	console.log(post2);
+	console.log('-----------------------');
+}
+
+driverScript();
+
+/* //const test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 const test = new Tree([1, 2, 3, 4, 5, 6, 7]);
 
 /* prettyPrint(test.root);
@@ -196,12 +293,11 @@ console.log('-------');
 insert(test.root, 8);
 prettyPrint(test.root);
 console.log('-------');
-deleteNode(test.root, 8); */
+deleteNode(test.root, 8);
 prettyPrint(test.root);
 console.log('-------');
 const test3 = depth(test, 7);
 console.log(test3);
-insert(test.root, 8);
 const test4 = depth(test, 8);
 console.log(test4);
 prettyPrint(test.root);
@@ -209,3 +305,25 @@ console.log('-------');
 const test5 = height(test.root, 6);
 console.log(test5);
 console.log('-------');
+const test6 = isBalanced(test.root);
+console.log(test6);
+insert(test.root, 8);
+insert(test.root, 800);
+insert(test.root, 900);
+insert(test.root, 1000);
+insert(test.root, 1100);
+insert(test.root, 1200);
+insert(test.root, 1300);
+insert(test.root, 1400);
+insert(test.root, 1500);
+insert(test.root, 1600);
+insert(test.root, 1700);
+console.log('-------');
+const test7 = rebalance(test.root);
+prettyPrint(test.root);
+console.log('-------');
+const test8 = isBalanced(test7.root);
+console.log(test8);
+console.log('-------');
+prettyPrint(test7.root);
+ */
