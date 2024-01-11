f74315be51e61872ac0db205a202ad97000d7265
JustWaveThings <justwavethings@proton.me>
2024-01-11 16:07:58 -0500

    modified methods for hashmap exercise

--- linked_list.js
+++ linked_list.js
@@ -1,160 +1,184 @@
-class LinkedList {
-	constructor(head = null) {
-		this.head = head;
-	}
+export class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
 
-	getHead() {
-		return this.head.value;
-	}
+  getHead() {
+    return this.head.value;
+  }
 
-	append(value) {
-		let node = new Node(value);
-		if (this.head === null) {
-			this.head = node;
-		} else {
-			let current = this.head;
-			while (current.next !== null) {
-				current = current.next;
-			}
-			current.next = node;
-		}
-	}
+  append(value) {
+    let node = new Node(value);
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
 
-	prepend(value) {
-		let node = new Node(value);
-		if (this.head === null) {
-			this.head = node;
-		} else {
-			node.next = this.head;
-			this.head = node;
-		}
-	}
+  prepend(value) {
+    let node = new Node(value);
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
 
-	size() {
-		let count = 0;
-		let current = this.head;
-		while (current !== null) {
-			count++;
-			current = current.next;
-		}
-		return count;
-	}
+  size() {
+    let count = 0;
+    let current = this.head;
+    while (current !== null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
 
-	tail() {
-		let current = this.head;
-		if (current === null) {
-			return null;
-		}
-		while (current.next !== null) {
-			current = current.next;
-		}
-		return current;
-	}
+  tail() {
+    let current = this.head;
+    if (current === null) {
+      return null;
+    }
+    while (current.next !== null) {
+      current = current.next;
+    }
+    return current;
+  }
 
-	at(index) {
-		let current = this.head;
-		let count = 0;
-		while (count !== index) {
-			count++;
-			current = current.next;
-		}
-		return current;
-	}
+  length() {
+    let length = 0;
+    let current = this.head;
+    if (current === null) {
+      return null;
+    }
+    while (current.next !== null) {
+      length++;
+      current = current.next;
+    }
+    return length;
+  }
 
-	pop() {
-		if (this.head === null) {
-			return null;
-		}
-		let current = this.head;
-		let previous = null;
+  at(index) {
+    let current = this.head;
+    let count = 0;
+    while (count !== index) {
+      count++;
+      current = current.next;
+    }
+    return current;
+  }
 
-		while (current.next !== null) {
-			previous = current;
-			current = current.next;
-		}
-		if (previous === null) {
-			this.head = null;
-		} else {
-			previous.next = null;
-		}
-		return current.data;
-	}
+  pop() {
+    if (this.head === null) {
+      return null;
+    }
+    let current = this.head;
+    let previous = null;
 
-	contains(value) {
-		let current = this.head;
+    while (current.next !== null) {
+      previous = current;
+      current = current.next;
+    }
+    if (previous === null) {
+      this.head = null;
+    } else {
+      previous.next = null;
+    }
+    return current.data;
+  }
 
-		while (current !== null) {
-			if (current.value === value) {
-				return true;
-			}
-			current = current.next;
-		}
-		return false;
-	}
+  contains(value) {
+    let current = this.head;
 
-	find(value) {
-		let count = 0;
-		let current = this.head;
+    while (current !== null) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
 
-		while (current !== null) {
-			if (current.value === value) {
-				return count;
-			}
-			count++;
-			current = current.next;
-		}
-		return null;
-	}
+  find(value) {
+    let count = 0;
+    let current = this.head;
 
-	toString() {
-		let current = this.head;
-		let string = '';
-		while (current !== null) {
-			string += `(${current.value}) -> `;
-			current = current.next;
-		}
-		string += 'null';
-		return string;
-	}
+    while (current !== null) {
+      if (current.value === value) {
+        return count;
+      }
+      count++;
+      current = current.next;
+    }
+    return null;
+  }
 
-	insertAt(value, index) {
-		let node = new Node(value);
-		let current = this.head;
-		let previous = null;
-		let count = 0;
-		while (count !== index) {
-			count++;
-			previous = current;
-			current = current.next;
-		}
-		previous.next = node;
-		node.next = current;
-	}
+  toString() {
+    let current = this.head;
+    let string = '';
+    while (current !== null) {
+      string += `(${current.value}) -> `;
+      current = current.next;
+    }
+    string += 'null';
+    return string;
+  }
 
-	removeAt(index) {
-		let current = this.head;
-		let previous = null;
-		let count = 0;
-		while (count !== index) {
-			count++;
-			previous = current;
-			current = current.next;
-		}
-		previous.next = current.next;
-	}
+  nodeValues() {
+    let current = this.head;
+    let values = [];
+    while (current !== null) {
+      values.push(current.value.value);
+      current = current.next;
+    }
+    return values;
+  }
+
+  insertAt(value, index) {
+    let node = new Node(value);
+    let current = this.head;
+    let previous = null;
+    let count = 0;
+    while (count !== index) {
+      count++;
+      previous = current;
+      current = current.next;
+    }
+    previous.next = node;
+    node.next = current;
+  }
+
+  removeAt(index) {
+    let current = this.head;
+    let previous = null;
+    let count = 0;
+    while (count !== index) {
+      count++;
+      previous = current;
+      current = current.next;
+    }
+    previous.next = current.next;
+  }
 }
 
-class Node {
-	constructor(value, next = null) {
-		this.value = value;
-		this.next = next;
-	}
+export class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 const list = new LinkedList();
 list.append(1);
 list.append(2);
 list.append(3);
+list.append('dasfdadsf');
 /* console.log(list.pop()); // undefined
 console.log(list.pop()); // undefined
 console.log(list.pop()); // undefined
@@ -177,4 +201,5 @@ list.insertAt('y', 1);
 
 // console.log(list.size()); // 3
 
-// console.log(list.getHead()); //  1
+// console.log(list.getHead()); //
+// console.log(list.length());
