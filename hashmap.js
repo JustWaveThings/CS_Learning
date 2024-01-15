b932b79513126bba2b1b71b712f898ba01b4c45d
JustWaveThings <justwavethings@proton.me>
2024-01-14 23:17:43 -0500

    Add log output

--- hashmap.js
+++ hashmap.js
@@ -8,9 +8,7 @@ function HashMap() {
   let buckets = Array.from({ length: capacity }, () => null);
 
   const insert = value => {
-    // check load ratio, if overloaded, expand array and set previous values
     manageLoadRatio();
-    // calc hash and key
     const hashValue = hash(value);
     const keyValue = hashValue.hashCode % capacity;
     set(keyValue, hashValue);
@@ -165,21 +163,55 @@ const names = [
   'var(--cody)',
   'zer0',
 ];
-// initializes a hashmap instance
 
 const hm = HashMap();
+
 names.forEach(name => {
   hm.insert(name);
 });
-// hm.logAfter();
-// console.log(hm.values().valueList);
-console.log(hm.length());
-console.log(hm.get(3));
-console.log(hm.has(3));
-console.log(hm.remove(3));
-console.log(hm.get(4));
-console.log(hm.remove(4));
-console.log(hm.get(4));
-console.log(hm.keys());
 
+console.log(hm.length()); //  25
+console.log(hm.get(3)); // null
+console.log(hm.has(3)); // false
+console.log(hm.remove(3)); // false
+console.log(hm.get(4));
+/*  [
+  { hashCode: 3421295364, value: 'timato' },
+  { hashCode: 104371452292, value: 'rlmoser' }
+] */
+console.log(hm.remove(4)); // true
+console.log(hm.get(4)); // null
+
+console.log(hm.keys());
+/* [
+   0,  6, 11, 16, 18, 24, 25,
+  26, 27, 28, 29, 34, 36, 41,
+  42, 44, 46, 48, 56, 59
+] */
 console.log(hm.entries());
+
+/* [
+  [ 0, { hashCode: 93924078142526300000, value: 'takinabradley' } ],
+  [ 6, { hashCode: 1986175942, value: 'Bender' } ],
+  [ 11, { hashCode: 61939106955, value: 'BriggsE' } ],
+  [ 16, { hashCode: 71442135952, value: 'Miggels' } ],
+  [ 16, { hashCode: 2193373712, value: 'JMyers' } ],
+  [ 18, { hashCode: 3565586, value: 'toby' } ],
+  [ 24, { hashCode: 2092632, value: 'Cake' } ],
+  [ 25, { hashCode: 74110105, value: 'Manon' } ],
+  [ 26, { hashCode: 3268186, value: 'josh' } ],
+  [ 27, { hashCode: 76596243227, value: 'Scheals' } ],
+  [ 28, { hashCode: 2011238172, value: 'Carlos' } ],
+  [ 29, { hashCode: 2297539101, value: 'Marvin' } ],
+  [ 34, { hashCode: 2402, value: 'KM' } ],
+  [ 36, { hashCode: 71645007012, value: 'Mr. ARL' } ],
+  [ 41, { hashCode: 3735145, value: 'zer0' } ],
+  [ 42, { hashCode: 2049706, value: 'Arun' } ],
+  [ 44, { hashCode: 2777580, value: 'Zach' } ],
+  [ 46, { hashCode: 99698465134, value: 'mdfr4nk' } ],
+  [ 48, { hashCode: 99379152616965040, value: 'var(--cody)' } ],
+  [ 56, { hashCode: 59300875135022840, value: 'Eduardo06sp' } ],
+  [ 59, { hashCode: 3377851, value: 'nevz' } ],
+  [ 59, { hashCode: 77115, value: 'Mao' } ],
+  [ 59, { hashCode: 1448818683, value: '105Ron' } ]
+] */
